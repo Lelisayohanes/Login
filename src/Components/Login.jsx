8f4dd4eb77568fe234dde52718c1db80911b7d04
Lelisa Yohannes Tarressa <lelisay@coopbank.local>
2023-07-28 11:00:01 +0000

    first login

---
+++ src/Components/Login.jsx
@@ -0,0 +1,29 @@
+import '../Styles/Login.css'
+
+function Login() {
+  return (
+    <div>
+      <div className="lg-form-container">
+        <div className="lg-form-header"></div>
+        <div className="lg-form-row">
+          <div className="lg-form-column">
+            <div className='lg-form-content'>
+              <h3 className='lg-form-h3'>LOGIN</h3>
+              <label className="lg-label">Email</label>
+              <input type="email" className="lg-input" placeholder="example@gmail.com" />
+              <label className="lg-label">Password</label>
+              <input
+                type="password"
+                className="lg-input"
+                placeholder="******"
+              />
+              <button className='lg-form-btn'>Login</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login
