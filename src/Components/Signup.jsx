cc45d5f13e82cd7eb72fb61264b8e5f2479950eb
Lelisa Yohannes Tarressa <lelisay@coopbank.local>
2023-07-28 13:54:04 +0000

    updated signup

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -7,16 +7,23 @@ function Login() {
         <div className="lg-form-header"></div>
         <div className="lg-form-row">
           <div className="lg-form-column">
-            <div className='lg-form-content'>
-              <h3 className='lg-form-h3'>Signup</h3>
+            <div className="lg-form-content">
+              <div className='lg-redirect-tab'>
+                <div className='lg-redirect-tab-signup'>
+                  <h3 className="lg-form-h3 "> <a className='redict-link' href='#'>Signup</a></h3>
+                </div>
+                <div className='lg-redirect-tab-signin'>
+                  <h3 className="lg-form-h3 "> <a className='redict-link' href='#'>Signin</a></h3>
+                </div>
+              </div>
               <label className="lg-label">Email</label>
-              <input type="email" className="lg-input" placeholder="example@gmail.com" />
-              <label className="lg-label">Phone</label>
               <input
-                type="Number"
+                type="email"
                 className="lg-input"
-                placeholder="******"
+                placeholder="example@gmail.com"
               />
+              <label className="lg-label">Phone</label>
+              <input type="Number" className="lg-input" placeholder="******" />
               <label className="lg-label">Password</label>
               <input
                 type="password"
@@ -29,7 +36,9 @@ function Login() {
                 className="lg-input"
                 placeholder="******"
               />
-              <button className='lg-form-btn'>Signup</button>
+              <div>
+                <button className="lg-form-btn">Signup</button>
+              </div>
             </div>
           </div>
         </div>
